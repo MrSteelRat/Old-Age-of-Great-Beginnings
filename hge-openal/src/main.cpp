037cf56cd5a4111f02cc2e093ee8045646f97fb3
MrSteelRat <drebin341@gmail.com>
2024-10-04 23:26:32 +0300

    add: HGE-Openal work

---
+++ hge-openal/src/main.cpp
@@ -0,0 +1,17 @@
+#include <chrono>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+bool LoadFile(const std::string_view file, )
+{
+
+}
+
+int main()
+{
+  cosnt auto sample = ("bell_host_oneshot_new.wav");
+}
